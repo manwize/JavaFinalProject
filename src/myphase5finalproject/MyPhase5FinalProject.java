f500dc0caf2ae39fe3fbdc53674bf7c85cdd535f
Marvin Little <manwize@live.com>
2015-05-04 01:26:38 -0400

    JavaFinalProject
    Java application with UML interface that allows user to make an order
    and the data is stored in a .txt file.

---
+++ src/myphase5finalproject/MyPhase5FinalProject.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myphase5finalproject;
+
+/**
+ *
+ * @author Tarik
+ */
+public class MyPhase5FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       ContactForm cform = new ContactForm();
+       cform.setVisible(true);
+
+    }
+    
+}
